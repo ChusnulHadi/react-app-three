ec96935b50428372300c2b85f1c3cc923be918e7
ChusnulHadi <office.chusnulhadi@gmail.com>
2022-03-13 22:16:34 +0700

    disable workspace, under maintenance

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-// import logo from './logo.svg';
-import GetStarted from './components/GetStarted';
 import './App.css';
-// import Frame from './components/Frame';
-import Workspace from './components/Workspace';
+
+import GetStarted from './components/GetStarted';
+// import Workspace from './components/Workspace';
 
 import styled from 'styled-components';
 
-// import background from './views/Frame1.svg';
 
 function App() {
   return (
     <Wrapper>
       <GetStarted />
-      <Workspace />
+      {/* <Workspace /> */}
     </Wrapper>
 
   );
